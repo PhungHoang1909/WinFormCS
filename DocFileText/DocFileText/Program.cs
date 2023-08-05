a31a3a6d5e5a1e9d4d75a53e7cb81792a5f47f0f
PhungHoang1909 <106148071+PhungHoang1909@users.noreply.github.com>
2023-08-05 10:55:13 +0700

    Add files via upload

---
+++ DocFileText/DocFileText/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"D:\LTGD\text.txt"; // Path to the input file
+
+        try
+        {
+            string content = File.ReadAllText(filePath);
+
+            Console.WriteLine("Contents of the file:");
+            Console.WriteLine(content);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found: " + filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadLine();
+    }
+}
